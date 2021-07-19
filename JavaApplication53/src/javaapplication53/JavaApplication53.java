a270d41ab77af70140278b10ca78a38cd352669a
habibagamal23 <habiba.gamaall23@gmail.com>
2021-07-19 02:06:49 +0200

    simple Code

--- JavaApplication53/src/javaapplication53/JavaApplication53.java
+++ JavaApplication53/src/javaapplication53/JavaApplication53.java
@@ -5,17 +5,60 @@
  */
 package javaapplication53;
 
-/**
- *
- * @author Dell
- */
-public class JavaApplication53 {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-    /**
-     * @param args the command line arguments
-     */
+public class JavaApplication53 {
+    
+    
+        static Connection con;
     public static void main(String[] args) {
-        // TODO code application logic here
+        ConnectClass db = new ConnectClass();
+        try {
+            con = db.getConnect();
+            PreparedStatement ps;
+            
+            ps=con.prepareStatement("INSERT INTO `register`( `name`, `course`) VALUES (?,?)");
+            ps.setString(1, "ragb");
+            ps.setString(2, "db");
+            ps.executeUpdate();
+           
+            
+            
+            System.out.println("Inserting Successfully  !");
+            
+            
+        } catch (SQLException e) {
+            e.getStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException sql) {
+                sql.getStackTrace();
+            }
+        }
     }
+
+}
+
+class ConnectClass {
+    
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 3306;
+    private static final String DB_NAME = "jdbc_course";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    private static final String URL = String.format("jdbc:mysql://localhost/studentdsc?serverTimezone=UTC");
+    static Connection con;
+    
+   public Connection getConnect() throws SQLException{
+
+       con=DriverManager.getConnection(URL, USER, PASS);
+       return con;
+   }
     
 }
